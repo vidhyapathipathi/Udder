f1acdb7e048a69ea7cbaa37c92c02c143473978e
vidhyapathipathi <vkandhasamy@aezion.com>
2018-05-10 13:03:38 +0530

    change UI Better

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
